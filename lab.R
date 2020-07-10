8b3493162c6eb21cb1c47b244e51362917f39ee4
Dan Hicks <hicks.daniel.j@gmail.com>
2020-07-10 10:20:32 -0700

    initial comit

---
+++ lab.R
@@ -0,0 +1,14 @@
+#' ---
+#' title: Data Science Methods, Lab Week 99
+#' author: Daniel J. Hicks
+#' email: hicks.daniel.j@gmail.com
+#' ---
+
+## Setup
+## **IMPORTANT**: Add all dependencies to `DESCRIPTION`
+
+## To check your answers locally, run the following: 
+## testthat::test_dir('tests')
+
+#' 1. Assign the value `2L` to `foo`
+foo = 2L
---
+++ tests/test_lab.R
@@ -0,0 +1,6 @@
+source("../lab.R", chdir = TRUE)
+library(testthat)
+
+test_that("1. Assign the value `2L` to `foo`", {
+    expect_identical(foo, 2L)
+})
