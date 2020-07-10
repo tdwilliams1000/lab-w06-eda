c715da1f71d0e946ccb55010b48f2a701fccb62a
Dan Hicks <hicks.daniel.j@gmail.com>
2020-07-10 10:44:08 -0700

    spacing and yaml header

--- lab.R
+++ lab.R
@@ -1,8 +1,12 @@
 #' ---
-#' title: Data Science Methods, Lab Week 99
-#' author: Daniel J. Hicks
+#' title: "Data Science Methods, Lab for Week 99"
+#' author: "Dan Hicks"
 #' email: hicks.daniel.j@gmail.com
+#' output:
+#'   html_document:
+#'     toc: true
 #' ---
+
 
 ## Setup
 ## **IMPORTANT**: Add all dependencies to `DESCRIPTION`
@@ -10,5 +14,8 @@
 ## To check your answers locally, run the following: 
 ## testthat::test_dir('tests')
 
-#' 1. Assign the value `2L` to `foo`
+
+
+#' # Problem 1 #
+#' Assign the value `2L` to `foo`. 
 foo = 2L
