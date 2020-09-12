e88df80509e4a4551ffe9020a3c6bfd20f0db449
Dan Hicks <hicks.daniel.j@gmail.com>
2020-09-12 09:11:58 -0700

    Initial commit

---
+++ lab.R
@@ -0,0 +1,21 @@
+#' ---
+#' title: "Data Science Methods, Lab for Week 99"
+#' author: "Your Name"
+#' email: Your Email
+#' output:
+#'   html_document:
+#'     toc: true
+#' ---
+
+
+## Setup
+## **IMPORTANT**: Add all dependencies to `DESCRIPTION`
+
+## To check your answers locally, run the following: 
+## testthat::test_dir('tests')
+
+
+
+#' # Problem 1 #
+#' Assign the value `2L` to `foo`. 
+foo = 2L
