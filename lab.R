6f189c8c84cf9ae8809e18531d6699fcfa6241b7
Dan Hicks <hicks.daniel.j@gmail.com>
2021-08-01 13:52:15 -0700

    add reflexivity questions (after Markham "Reflexivity: Some techniques for interpretive researchers," as quoted by Tanweer et al., "Why the data revolution needs qualitative thinking"

--- lab.R
+++ lab.R
@@ -7,15 +7,23 @@
 #'     toc: true
 #' ---
 
+#' # Introduction #
+#' 
 
-## Setup
-## **IMPORTANT**: Add all dependencies to `DESCRIPTION`
+#' # Reflexivity #
+#' *Before starting the lab, spend 5 minutes writing a response to each reflexivity question.  Use a timer.  Answer these questions off the top of your head: don't worry about consulting or citing outside sources or about getting the answers "right" or "wrong."* 
+#' 1. *What do I already know about this subject?*
+#' 2. *Why am I studying this?*
+#' 3. *What do I expect or hope to find, and why?*
+#' 4. *Who is effected by this topic, and how am I connected to them?* 
+#' 
+
+#' # Setup #
+#' **IMPORTANT**: Add all dependencies to `DESCRIPTION`
 
 ## To check your answers locally, run the following: 
 ## testthat::test_dir('tests')
 
-
-
 #' # Problem 1 #
-#' Assign the value `2L` to `foo`. 
+#' *Assign the value `2L` to `foo`.* 
 foo = 2L
