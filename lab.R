edfc49ef9f09ee5c1630beb3cbec8a56a31c8f3b
Dan Hicks <hicks.daniel.j@gmail.com>
2021-09-24 09:57:41 -0700

    spelling

--- lab.R
+++ lab.R
@@ -14,8 +14,8 @@
 #' *Before starting the lab, spend 5 minutes writing a response to each reflexivity question.  Use a timer.  Answer these questions off the top of your head: don't worry about consulting or citing outside sources or about getting the answers "right" or "wrong."* 
 #' 1. *What do I already know about this subject?*
 #' 2. *Why am I studying this?*
-#' 3. *What do I expect or hope to find, and why?*
-#' 4. *Who is effected by this topic, and how am I connected to them?* 
+#' 3. *What do I expect or hope to find/learn, and why?*
+#' 4. *Who is affected by this topic, and how am I connected to them?* 
 #' 
 
 #' # Setup #
