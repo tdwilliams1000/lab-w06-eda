056f8d78065571c3f53ff4cace7a5eedfc279610
Dan Hicks <hicks.daniel.j@gmail.com>
2021-09-25 13:26:52 -0700

    7.1

--- lab.R
+++ lab.R
@@ -188,7 +188,7 @@ dataf = mutate(dataf, across(.cols = c(exter.cond, bsmt.cond,
 
 #' # Problem 7 #
 # problem 7 ----
-#' *Recall that we're interested in finding variables that are highly correlated with sale price.  We can use the function `cor()` to construct a correlation matrix, with correlations between all pairs of variables in the dataframe.  But this creates two challenges.  First, `cor()` only works with numerical inputs.  If we try it with our current dataframe, it throws an error*:  
+#' *Recall that we're interested in finding variables that are highly correlated with sale price.  We can use the function `cor()` to construct a correlation matrix, with correlations between all pairs of variables in the dataframe.  But this creates two challenges.  First, `cor()` only works with numerical inputs.  If we try it with our current dataframe, it throws an error:*  
 
 # cor(dataf)
 
